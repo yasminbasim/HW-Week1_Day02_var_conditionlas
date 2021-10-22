78147741e8ec94f5101b7b7f636f9a55f9e32762
yasminbasim <Yasmin.Basim.jed.k2@tuwaiq.edu.sa>
2021-10-22 08:09:03 +0300

    (:

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,18 +1,27 @@
-//part2
-fun main(){
- val player1="player1"
- val lifePontPlayer1=100
- val player2="player2"
- val lifePointPlyer2=45
+// Write a kotlin program that has two `players` with there `healthPoints` and let the program
+// check if the healthPoint is less than 90 out 100 print `the player's health is fine` and if
+// the healthPoint is less than 50 print `the player's health is awful
 
- if (lifePontPlayer1>=90) {
+//part2
+fun main() {
+ val player1 = "player1"
+ val lifePontPlayer1 = 100
+ val player2 = "player2"
+ val lifePointPlyer2 = 45
+
+ if (lifePontPlayer1>90) {
   println("player $player1 your health is fine")
  } else {
   println("player $player1  your healthing ")
  }
+
  if (lifePointPlyer2>= 50) {
   println("player $player2 your need healthing ")
  }else {
   println("player $player2 your health is awful")
  }
+
+
+
+
 }
